77a03e438f7f028814be65041f6e823f1d519adb
aloctavodia <aloctavodia@gmail.com>
2017-03-29 09:33:08 -0300

    add instalation setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='bomeba0',
+      version='0.0.1',
+      description='yet another macromolecular package',
+      author='Agustina Arroyuelo and Osvaldo Martin',
+      author_email='agustinaarroyuelo@gmail.com, aloctavodia@gmail.com',
+      url='https://github.com/bomeba/bomeba0',
+      packages=['bomeba0'],
+     )
